9436acfacee5f4eb9f607bfe202aead0b936f464
Nuria Tired <nuriatired@gmail.com>
2019-01-09 19:10:24 +0100

    Revision 9Enero Funcionando

--- NTmars-rover.js
+++ NTmars-rover.js
@@ -1,155 +1,180 @@
-
 // Rover Object Goes Here
 // ======================
+
 var rover = {
-  direction: "N",
-  row:0,
-  column:0, 
-  position: [0, 0],
-  travelLog: []
+    "direction": "N",
+    "row": 0,
+    "column": 0,
+    "travelLog": ["0,0"]
 };
+var travel = "rffrfflfrffblbb";
+
 // ======================
 
-//buscar la manera de hacerlo mas eficiente
-var newPosition;
-
-function free(c) {
-    if (obstacles[newPosition][c] == " ") {
-        console.log("No obstacle " + " Row:" + newPosition + "Column: "+ c);
+function free(obstacles, r, c) {
+    if (obstacles[r][c] == " ") {
+        console.log("No obstacle", "Row:", r, "Column: ", c);
         return true;
     }
     else {
-        console.log("Obstaculo detectado: "+ obstacles+"["+newPosition+"]"+"["+ c +"]" + "Row: "+ newPosition + "Column: ", c);
+        console.log("Obstacle: ", obstacles[r][c], "Row", r, "Column", c);
         return false;
 
     }
 }
 
-function avanzar() {
-    if ((newPosition >= 0 || newPosition <= 9) && (free(rover.column))) {
-        rover.row = newPosition;
-        mensaje = "Rover se mueve hacia delante";
-    }
-    else { mensaje = "Rover no puede moverse por obstaculo, o porque se sale del mapa"; }
-
-}
-
-function retroceder() {
-    if ((newPosition >= 0 || newPosition <= 9) && (free(rover.row))) {
-        rover.row = newPosition;
-        mensaje = "Rover retrocede";
-    }
-    else { mensaje = "Rover no puede moverse por obstaculo, o porque se sale del mapa"; }
-
-}
-//buscar la manera de hacerlo mas eficiente
 
 
-
-
-
-
-//Turn Left
+// ======================GIRAR IZQUIERDA
 function turnLeft(rover) {
     switch (rover.direction) {
         case "N":
             rover.direction = "W";
             break;
+
         case "W":
             rover.direction = "S";
             break;
+
         case "S":
             rover.direction = "E";
             break;
+
         case "E":
             rover.direction = "N";
             break;
     }
-    console.log("turnLeft was called!");
+    console.log("turnLeft was called!", rover.direction);
 }
-//Turn Right
+
+// ======================GIRAR DERECHA
 function turnRight(rover) {
     switch (rover.direction) {
-        case "N": rover.direction = "E"; break;
-        case "E": rover.direction = "S"; break;
-        case "S": rover.direction = "W"; break;
-        case "W": rover.direction = "N"; break;
+        case "N":
+            rover.direction = "E";
+            break;
+
+        case "E":
+            rover.direction = "S";
+            break;
+
+        case "S":
+            rover.direction = "W";
+            break;
+
+        case "W":
+            rover.direction = "N";
+            break;
     }
-    console.log("turnRight was called!");
+    console.log("turnRight was called!", rover.direction);
 }
 
 
-//-----------------AVANZAR
-
+// ======================MOVER HACIA DELANTE
 function moveForward(rover, obstacles) {
-    var mensaje;
+    var newPosition;
+    var mensaje = "Rover was moved Forward";
+    function paraRow() {
+        if ((newPosition >= 0 || newPosition <= 9) && free(obstacles, newPosition, rover.column)) {
+            rover.row = newPosition;
+            mensaje;
+        } else {
+            mensaje = "Rover can`t move. Maybe there's an obstacle.Rover goes off the map";
+        }
+    };
+
+    function paraColumn() {
+        if ((newPosition >= 0 || newPosition <= 9) && free(obstacles, rover.row, newPosition)) {
+            rover.column = newPosition;
+            mensaje;
+        } else {
+            mensaje = "Rover can`t move. Maybe there's an obstacle.Rover goes off the map";
+        }
+    };
 
     switch (rover.direction) {
         case "N":
             newPosition = rover.row - 1;
-            avanzar();
+            paraRow();
             break;
 
         case "E":
             newPosition = rover.column + 1;
-            avanzar();
+            paraColumn();
             break;
 
         case "S":
             newPosition = rover.row + 1;
-            avanzar();
+            paraRow();
             break;
 
         case "W":
             newPosition = rover.column - 1;
-            avanzar();
+            paraColumn();
             break;
     }
 
     console.log(mensaje, rover);
 
-    if (mensaje == "Rover se mueve hacia delante") {
-      rover.travelLog.push(rover.row + "," + rover.column);
+    if (mensaje == "Rover was moved Forward") {
+        rover.travelLog.push(rover.row + "," + rover.column);
     }
 }
 
-//-----------------RETROCEDER
 
+// ======================MOVER HACIA ATRAS
 function moveBackward(rover, obstacles) {
-    var mensaje;
-    
+    var newPosition;
+    var mensaje = "Rover was moved Backward";
+    function paraRow() {
+        if ((newPosition >= 0 || newPosition <= 9) && free(obstacles, newPosition, rover.column)) {
+            rover.row = newPosition;
+            mensaje;
+        } else {
+            mensaje = "Rover can`t move. Maybe there's an obstacle.Rover goes off the map";
+        }
+    };
+
+    function paraColumn() {
+        if ((newPosition >= 0 || newPosition <= 9) && free(obstacles, rover.row, newPosition)) {
+            rover.column = newPosition;
+            mensaje;
+        } else {
+            mensaje = "Rover can`t move. Maybe there's an obstacle.Rover goes off the map";
+        }
+    };
+
     switch (rover.direction) {
         case "N":
             newPosition = rover.row + 1;
-            retroceder();
+            paraRow();
             break;
 
         case "E":
             newPosition = rover.column - 1;
-            retroceder();
+            paraColumn();
             break;
 
         case "S":
             newPosition = rover.row - 1;
-            retroceder();
+            paraRow();
             break;
 
         case "W":
             newPosition = rover.column + 1;
-            retroceder();
+            paraColumn();
             break;
     }
 
     console.log(mensaje, rover);
 
-    if (mensaje == "Rover retrocede") {
-      rover.travelLog.push(rover.row + "," + rover.column);
+    if (mensaje == "Rover was moved Backward") {
+        rover.travelLog.push(rover.row + "," + rover.column);
     }
 }
 
 
-//-----------------COMANDOS
-
+// ======================COMANDOS PARA MOVER
 function movimientos(rover, move) {
 
     for (var i = 0; i < move.length; i++) {
@@ -169,14 +194,13 @@ function movimientos(rover, move) {
             case "l":
                 turnLeft(rover);
                 break;
-
-            default: console.log("Invalid movement", rover);
+            case "0":
+                break;
+            default: console.log("To move the rover use the keys b,f,r/l,try again! or press 0 to exit.");
         }
     }
 
 }
-
-
 
 var obstacles = [
     [' ', 'R', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -191,3 +215,9 @@ var obstacles = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']];
 
 console.log(obstacles);
+
+
+console.log("movement", travel);
+
+movimientos(rover, travel);
+console.log("Recorrido: ", rover.travelLog);
